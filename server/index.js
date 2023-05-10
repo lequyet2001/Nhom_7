dcc38e3b18d0b31084fe5ebc1cffc5467a6bff79
quyet <quyet.lh198327@sis.hust.edu.vn>
2023-05-11 05:31:26 +0700

    6

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('Server đang chạy trên cổng 3000!')
+})
